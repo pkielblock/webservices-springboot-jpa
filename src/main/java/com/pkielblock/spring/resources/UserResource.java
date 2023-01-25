cbbffdd858f0d46843bd972b2462e45325e873f5
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-25 14:17:20 -0300

    User Entity and Resource

---
+++ src/main/java/com/pkielblock/spring/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.pkielblock.spring.resources;
+
+import com.pkielblock.spring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
+        return ResponseEntity.ok().body(user);
+    }
+}

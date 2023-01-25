85304b13e6d5a6a19e4634690b60d2265a33bef2
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-25 18:45:15 -0300

    methods subtotal and total

--- src/main/java/com/pkielblock/spring/entities/Payment.java
+++ src/main/java/com/pkielblock/spring/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.pkielblock.spring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,7 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
 
     public Payment() {

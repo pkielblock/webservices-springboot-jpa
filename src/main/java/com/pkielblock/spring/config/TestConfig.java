1a362cc1d9e6eaad5da84ba90d00baae969d4c54
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-25 18:05:49 -0300

    OrderItem

--- src/main/java/com/pkielblock/spring/config/TestConfig.java
+++ src/main/java/com/pkielblock/spring/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.pkielblock.spring.config;
 
-import com.pkielblock.spring.entities.Category;
-import com.pkielblock.spring.entities.Order;
-import com.pkielblock.spring.entities.Product;
-import com.pkielblock.spring.entities.User;
+import com.pkielblock.spring.entities.*;
 import com.pkielblock.spring.entities.enums.OrderStatus;
-import com.pkielblock.spring.repositories.CategoryRepository;
-import com.pkielblock.spring.repositories.OrderRepository;
-import com.pkielblock.spring.repositories.ProductRepository;
-import com.pkielblock.spring.repositories.UserRepository;
+import com.pkielblock.spring.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +22,9 @@ public class TestConfig implements CommandLineRunner {
     private CategoryRepository categoryRepository;
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -50,14 +47,18 @@ public class TestConfig implements CommandLineRunner {
         Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
         Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-
         p1.getCategories().add(cat2);
         p2.getCategories().add(cat1);
         p2.getCategories().add(cat3);
         p3.getCategories().add(cat3);
         p4.getCategories().add(cat3);
         p5.getCategories().add(cat2);
-
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
---
+++ src/main/java/com/pkielblock/spring/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.pkielblock.spring.repositories;
+
+import com.pkielblock.spring.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}

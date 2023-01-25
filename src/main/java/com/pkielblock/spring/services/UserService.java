ba45ec6980475639291a20a00bf0442c8c070712
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-25 19:52:10 -0300

    findById exception

--- src/main/java/com/pkielblock/spring/services/UserService.java
+++ src/main/java/com/pkielblock/spring/services/UserService.java
@@ -2,6 +2,7 @@ package com.pkielblock.spring.services;
 
 import com.pkielblock.spring.entities.User;
 import com.pkielblock.spring.repositories.UserRepository;
+import com.pkielblock.spring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
